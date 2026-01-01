071876332e8106892566b4f67155d1afa2b7681e
fahadewu <fahad@oracrondigital.com>
2026-01-02 01:45:40 +0600

    shifted to smtp because of the google authentications shows error after a few otp sending

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -29,7 +29,9 @@ const generateOTP = async (req, res) => {
         // Send OTP via Email to user
         try {
             const transporter = nodemailer.createTransport({
-                service: 'gmail',
+                host: process.env.SMTP_HOST ,
+                port: process.env.SMTP_PORT || 465,
+                secure: true, // true for 465, false for other ports
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASS
